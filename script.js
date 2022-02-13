3752911ac3f1f20735a88663f6836654eec6825c
NurayymJeenbekova <nurayym.jeenbekova.kk@gmail.com>
2022-02-13 15:30:49 +0600

    js

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".form");
+    const input = document.querySelector("#input");
+    const list = document.querySelector("#result");
+    const btn = document.querySelector("#button");
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value;
+        input.value ="";
+
+        if (!task) {
+            alert("Enter tasks and click to 'Add Todo'");
+            return;
+        }
+
+        const t1 = document.createElement("div");
+        t1.classList.add("task");
+        const t2 = document.createElement("div");
+        t2.classList.add("todo-item");
+        t2.innerText=task;
+
+        t1.appendChild(t2);
+
+        list.appendChild(t1);
+        t1.addEventListener('click', () => {
+            list.removeChild(t1);
+        });
+
+    })
